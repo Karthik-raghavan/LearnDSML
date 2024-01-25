cc2fcc265fd48984589eefb7a54e4459990b080c
Karthikraghavan <kr.ra.ak@outlook.com>
2024-01-25 15:39:27 +0530

    Introduction to R Basics

---
+++ LearnR/Introduction to R Basics/Variables.R
@@ -0,0 +1,17 @@
+print('Hello') # printing Hello
+
+# variables
+
+variable_name <- 5
+print(variable_name)
+
+bankAccount <- 1000
+print(bankAccount)
+bankAccount
+
+
+# Calculate
+
+deposit <- 20
+bankAccount <- bankAccount + deposit
+print(bankAccount)
