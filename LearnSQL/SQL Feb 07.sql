8c6d615733536ff5668744e5a128cc22d6d47107
Karthikraghavan <kr.ra.ak@outlook.com>
2024-02-07 14:23:12 +0530

    Feb07 SQL class

---
+++ LearnSQL/SQL Feb 07.sql
@@ -0,0 +1,10 @@
+# Query Optimization
+# 1. What does Query Optimization mean ?
+# 2. 2 FB interview Question
+# 3. Target Business Case
+
+USE employees;
+
+# Optimization
+
+# trend - change over time - time series analysis
